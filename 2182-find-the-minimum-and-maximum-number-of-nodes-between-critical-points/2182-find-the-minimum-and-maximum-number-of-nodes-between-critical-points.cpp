eabe1cd2ea2ca3ff9176aacd4e48a1640b7be516
Rajesh Kumar <112788912+Rajesh351@users.noreply.github.com>
2024-07-05 11:19:49 +0530

    Time: 170 ms (46.44%), Space: 119.8 MB (11.59%) - LeetHub

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -11,46 +11,39 @@
 class Solution {
 public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
-       
 
-        ListNode *prev=head;
-        ListNode *curr=head->next;
-        if(curr->next==NULL) return {-1,-1};
-        ListNode *next=curr->next;
-         int count=2;
-         vector<int>v;
-        while(curr->next!=NULL)
-        {
-            if(prev->val<curr->val && curr->val>next->val)
-             {
+        ListNode* prev = head;
+        ListNode* curr = head->next;
+        if (curr->next == NULL)
+            return {-1, -1};
+        ListNode* next = curr->next;
+        int count = 2;
+        vector<int> v;
+        while (curr->next != NULL) {
+            if (prev->val < curr->val && curr->val > next->val) {
                 v.push_back(count);
-             }
-            
-            else if(prev->val>curr->val && curr->val <next->val)
-            {
+            }
+
+            else if (prev->val > curr->val && curr->val < next->val) {
                 v.push_back(count);
             }
             count++;
-            prev=curr;
-            curr=curr->next;
-            if(curr->next!=NULL) next=curr->next;
+            prev = curr;
+            curr = curr->next;
+            if (curr->next != NULL)
+                next = curr->next;
         }
-         
-         if(v.size()>1)
-         {
-        int maxdis=abs(v[0]-v[v.size()-1]);
-        int mindis=INT_MAX;
-        for(int i=1;i<v.size();i++)
-        {
-            mindis=min(mindis,v[i]-v[i-1]);
+
+        if (v.size() > 1) {
+            int maxdis = abs(v[0] - v[v.size() - 1]);
+            int mindis = INT_MAX;
+            for (int i = 1; i < v.size(); i++) {
+                mindis = min(mindis, v[i] - v[i - 1]);
+            }
+            if(maxdis>mindis) return {mindis,maxdis};
+
+            return {maxdis,mindis};
         }
-        vector<int>v1;
-        v1.push_back(maxdis);
-        v1.push_back(mindis);
-        sort(v1.begin(),v1.end());
-        return v1;
-         }
-         return {-1,-1};
-        
+        return {-1, -1};
     }
 };
