aae5c93e4f806bfb10e0d4803176a0cae6231c6b
Rajesh Kumar <112788912+Rajesh351@users.noreply.github.com>
2024-07-09 09:47:55 +0530

    Time: 142 ms (44.03%), Space: 131.6 MB (50.82%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-       vector<int>small,big;
-       int count=0;
-       for(auto a:nums)
-       {
-         if(pivot>a) small.push_back(a);
-         else if(pivot==a) count++;
-         else big.push_back(a);
-       }
-        vector<int>ans;
-        for(int i=0;i<small.size();i++) ans.push_back(small[i]);
-        for(int i=0;i<count;i++) ans.push_back(pivot);
-        for(int j=0;j<big.size();j++) ans.push_back(big[j]);
-        return ans;
+    
+        
+        vector<int> final1;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]<pivot){
+                final1.push_back(nums[i]);
+            }
+        }
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==pivot){
+                final1.push_back(nums[i]);
+            }
+        }
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]>pivot){
+                final1.push_back(nums[i]);
+            }
+        }
+        
+       
 
+        return final1;
     }
 };
