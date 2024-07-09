2bf96dfcc34c8f3ffbc1b747a48c5ff83f01bc11
Rajesh Kumar <112788912+Rajesh351@users.noreply.github.com>
2024-07-09 09:24:27 +0530

    Time: 0 ms (100%), Space: 7.3 MB (91.21%) - LeetHub

--- 2327-largest-number-after-digit-swaps-by-parity/2327-largest-number-after-digit-swaps-by-parity.cpp
+++ 2327-largest-number-after-digit-swaps-by-parity/2327-largest-number-after-digit-swaps-by-parity.cpp
@@ -2,27 +2,18 @@ class Solution {
 public:
     int largestInteger(int num) {
         
-        vector<int>v;
-        while(num>0)
+        string str=to_string(num);
+        for(int i=0;i<str.size();i++)
         {
-            v.push_back(num%10);
-            num=num/10;
-        }
-        reverse(v.begin(),v.end());
-        for(int i=0;i<v.size()-1;i++)
-        {
-            for(int j=i+1;j<v.size();j++)
+            bool even=(str[i]-'0')%2==0;
+            for(int j=i+1;j<str.size();j++)
             {
-                if((v[i]%2==0 && v[j]%2==0) && v[i]<v[j]) swap(v[i],v[j]);
-                if((v[i]%2!=0 && v[j]%2!=0) && v[i]<v[j]) swap(v[i],v[j]);
+                if(even==(str[j]-'0')%2==0)
+                {
+                    if(str[i]<str[j]) swap(str[i],str[j]);
+                }
             }
         }
-          reverse(v.begin(),v.end());
-        int sum=0;
-        for(int i=v.size()-1;i>=0;i--)
-        {
-           sum=sum*10+v[i];
-        }
-        return sum;
+           return stoi(str);
     }
 };
