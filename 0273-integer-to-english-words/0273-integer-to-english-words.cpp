3f39ef47ff794687ea348f65df60414c14d6dda9
Rajesh Kumar <112788912+Rajesh351@users.noreply.github.com>
2024-08-11 19:25:38 +0530

    Time: 8 ms (12.82%), Space: 12.5 MB (18%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.cpp
+++ 0273-integer-to-english-words/0273-integer-to-english-words.cpp
@@ -1,59 +1,23 @@
 class Solution {
 public:
- const string oneToNineteen[20] = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
-const string tens[10] = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
-const string thousands[4] = {"", "Thousand", "Million", "Billion"};
-
-string convertHundred(int n) {
-    string result;
-    if (n / 100 > 0) {
-        result += oneToNineteen[n / 100] + " Hundred ";
-        n %= 100;
-    }
-    if (n >= 20) {
-        result += tens[n / 10] + " ";
-        n %= 10;
-    }
-    if (n > 0) {
-        result += oneToNineteen[n] + " ";
-    }
-    return result;
-}
-
-string convertToWords(long long n) {
-    if (n == 0) {
-        return "Zero";
+   vector<string> belowTwenty = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", 
+                                  "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", 
+                                  "Eighteen", "Nineteen"};
+    vector<string> tens = {"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+    vector<string> thousands = {"", "Thousand", "Million", "Billion"};
+    string numberToWords(int num) {
+        if (num == 0) return "Zero";
+        return convert(num).substr(1); // Remove the leading space
     }
 
-    string result;
-    int i = 0;
-    
-    while (n > 0) {
-        if (n % 1000 != 0) {
-            result = convertHundred(n % 1000) + thousands[i] + " " + result;
-        }
-        n /= 1000;
-        i++;
+private:
+    string convert(int num) {
+        if (num == 0) return "";
+        if (num < 20) return " " + belowTwenty[num];
+        if (num < 100) return " " + tens[num / 10] + convert(num % 10);
+        if (num < 1000) return " " + belowTwenty[num / 100] + " Hundred" + convert(num % 100);
+        if (num < 1000000) return convert(num / 1000) + " Thousand" + convert(num % 1000);
+        if (num < 1000000000) return convert(num / 1000000) + " Million" + convert(num % 1000000);
+        return convert(num / 1000000000) + " Billion" + convert(num % 1000000000);
     }
-
-    // Capitalize the first letter of the result
-    if (!result.empty()) {
-        result[0] = toupper(result[0]);
-    }
-
-    // Remove trailing space
-    bool check=true;
-    while(check)
-    {
-    if (result.back() == ' ') {
-        result.pop_back();
-    }
-    else check=false;}
-
-    return result;
-}
-    string numberToWords(int num) { 
-    string str=convertToWords(num);
-       return str;  
-     }
 };
