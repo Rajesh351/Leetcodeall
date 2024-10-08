153905537cc7c3991e0efa25bc26824744a89735
Rajesh Kumar <112788912+Rajesh351@users.noreply.github.com>
2024-10-08 21:24:23 +0530

    Time: 95 ms (87.14%), Space: 32.7 MB (79.33%) - LeetHub

---
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+   int minSwaps(string s) {
+    int imbalance = 0;
+    int swaps = 0;
+    
+    for (char c : s) {
+        if (c == '[') {
+            imbalance++;  
+        } else {
+            imbalance--; 
+        }
+        
+        if (imbalance < 0) { 
+            swaps++;  
+            imbalance = 1; 
+        }
+    }
+    
+    return swaps;
+}
+
+};
