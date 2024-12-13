983b495baa95ef75c432b36074ad23402438d4aa
Rajesh Kumar <112788912+Rajesh351@users.noreply.github.com>
2024-12-13 11:52:01 +0530

    Time: 292 ms (22.02%), Space: 150.5 MB (18.95%) - LeetHub

--- 2695-find-score-of-an-array-after-marking-all-elements/2695-find-score-of-an-array-after-marking-all-elements.cpp
+++ 2695-find-score-of-an-array-after-marking-all-elements/2695-find-score-of-an-array-after-marking-all-elements.cpp
@@ -9,31 +9,22 @@ public:
               minHeap.push({nums[i],i});
           }
           
+           long long score=0;
 
-       long long score=0;
-         while (mp.size() < nums.size()) {
-        if (minHeap.empty()) break;
-
-        auto it = minHeap.top();
-        minHeap.pop();
-
-        int value = it.first;
-        int index = it.second;
-
-        // Skip if the current index is already marked
-        if (mp.find(index) != mp.end()) {
-            continue;
-        }
-
-        // Add the value to the score
-        score += value;
-
-        // Mark the current index and adjacent indices
-        mp[index]++;
-        if (index - 1 >= 0) mp[index - 1]++;
-        if (index + 1 < nums.size()) mp[index + 1]++;
-    }
-        return score;  
+          while(mp.size()<nums.size())
+          {
+             if(minHeap.empty()) break;
+          auto it=minHeap.top();
+          minHeap.pop();
+          if(mp.find(it.second)!=mp.end()) continue;
+          
+            score+=it.first;
+          
+           mp[it.second]++;
+          if(it.second-1>=0)mp[it.second-1]++;
+          if(it.second+1<=nums.size()-1)  mp[it.second+1]++;
+          }
+          return score;
 
     }
 };
