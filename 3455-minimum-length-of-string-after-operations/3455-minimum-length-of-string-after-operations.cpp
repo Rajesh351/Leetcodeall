2e705f3b34e873253de89bcd25c61787abef41fa
Rajesh Kumar <112788912+Rajesh351@users.noreply.github.com>
2025-01-13 08:50:13 +0530

    Time: 109 ms (27.02%), Space: 32.5 MB (36.69%) - LeetHub

--- 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.cpp
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.cpp
@@ -2,17 +2,20 @@ class Solution {
 public:
     int minimumLength(string s) {
         
-       int n = s.length(), del = 0;
-        unordered_map<char, int> mp;
-        // Count the frequency of each character
-        for(int i = 0; i < n; i++){
-            mp[s[i]]++;
-        }
-        // Calculate the number of characters to be removed
-        for(auto ele : mp){
-            int freq = ele.second;
-            del += (freq%2) ? freq - 1 : freq - 2;
-        }
-        return n - del;
+      unordered_map<char,int>mp;
+      for(char a:s)
+      {
+         mp[a]++;
+         if(mp[a]==3)
+         {
+            mp[a]=1;
+         }
+      }
+      int ans=0;
+      for(auto it:mp)
+      {
+        ans+=it.second;
+      }
+      return ans;
     }
 };
