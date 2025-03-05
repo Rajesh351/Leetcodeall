071ff6ef41b5f30a69725211009054ac8e79a335
Rajesh Kumar <112788912+Rajesh351@users.noreply.github.com>
2025-03-05 10:20:00 +0530

    Time: 0 ms (100%), Space: 7.9 MB (61.42%) - LeetHub

--- 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
@@ -3,7 +3,8 @@ public:
     long long coloredCells(int n) {
         
       if(n==1) return 1;
-    return 1LL * n * n + 1LL * (n - 1) * (n - 1);
+
+   return 1LL * n * n + 1LL * (n - 1) * (n - 1);
 
     }
 };
